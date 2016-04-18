eca549e9b34b7108664d40adf318eca0b6356ba7
Taco de Wolff <tacodewolff@gmail.com>
2016-04-18 15:59:10 +0200

    Add That

--- test.go
+++ test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"runtime"
@@ -34,29 +35,35 @@ func printable(s string) string {
 	return s
 }
 
-func Assert(t *testing.T, condition bool, msgs ...string) {
+func That(t *testing.T, condition bool, msg ...interface{}) {
 	if !condition {
-		t.Errorf("%s: %s\n", trace(), strings.Join(msgs, " "))
+		t.Errorf("%s: %s\n", trace(), fmt.Sprint(msg...))
 	}
 }
 
-func Error(t *testing.T, err, expected error, msgs ...string) {
+func Error(t *testing.T, err, expected error, msg ...interface{}) {
 	if err != expected {
-		t.Errorf("%s: %s\n   error: %v\nexpected: %v\n", trace(), strings.Join(msgs, " "), err, expected)
+		t.Errorf("%s: %s\n   error: %v\nexpected: %v\n", trace(), fmt.Sprint(msg...), err, expected)
 	}
 }
 
-func String(t *testing.T, output, expected string, msgs ...string) {
+func String(t *testing.T, output, expected string, msg ...interface{}) {
 	if output != expected {
-		t.Errorf("%s: %s\nminified: %s\nexpected: %s\n", trace(), strings.Join(msgs, " "), printable(output), printable(expected))
+		t.Errorf("%s: %s\n  output: %s\nexpected: %s\n", trace(), fmt.Sprint(msg...), printable(output), printable(expected))
 	}
 }
 
-func Minify(t *testing.T, input string, err error, output, expected string, msgs ...string) {
+func Bytes(t *testing.T, output, expected []byte, msg ...interface{}) {
+	if !bytes.Equal(output, expected) {
+		t.Errorf("%s: %s\n  output: %s\nexpected: %s\n", trace(), fmt.Sprint(msg...), printable(string(output)), printable(string(expected)))
+	}
+}
+
+func Minify(t *testing.T, input string, err error, output, expected string, msg ...interface{}) {
 	if err != nil {
-		t.Errorf("%s: %s\n   given: %s\n   error: %v\n", trace(), strings.Join(msgs, " "), printable(input), err)
+		t.Errorf("%s: %s\n   given: %s\n   error: %v\n", trace(), fmt.Sprint(msg...), printable(input), err)
 	}
 	if output != expected {
-		t.Errorf("%s: %s\n   given: %s\nminified: %s\nexpected: %s\n", trace(), strings.Join(msgs, " "), printable(input), printable(output), printable(expected))
+		t.Errorf("%s: %s\n   given: %s\nminified: %s\nexpected: %s\n", trace(), fmt.Sprint(msg...), printable(input), printable(output), printable(expected))
 	}
 }
